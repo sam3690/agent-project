b3eebae902181f9a1b923a73d72729523f8afe88
Usama Ayoub <usamabinayoub@gmail.com>
2025-06-13 17:51:24 +0500

    m

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
 import os
 from pydantic import BaseModel
-from openai import OpenAI
+# from openai import OpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import PydanticOutputParser
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+
 
 load_dotenv()
 
@@ -17,10 +20,13 @@ class ResearchResponse(BaseModel):
     sources: list[str]
     tools_used: list[str]
 
-llm = OpenAI(
-  base_url="https://openrouter.ai/api/v1",
-  api_key=OPENROUTE_API_KEY,
+llm = ChatOpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=OPENROUTE_API_KEY,
+    model="deepseek/deepseek-chat",  # Specify the model you want to use
+    temperature=0.1,
 )
+
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
 prompt = ChatPromptTemplate.from_messages(
@@ -28,7 +34,8 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """You are a helpful research assistantthat will help generate a research paper.
-            Answer the user query and use neccessary tools.""",
+            Answer the user query and use neccessary tools.
+            Wrap the output in this format and provide no other text.\n{{format_instruction}}""",
         ),
         (
             "placeholder",
@@ -36,14 +43,24 @@ prompt = ChatPromptTemplate.from_messages(
         ),
         (
             "human",
-            "{query}"
+            "{input}"
         ),
         (
             "placeholder",
-            "{topic}",
+            "{agent_scratchpad}",
         )
     ]
 ).partial(format_instructions=parser.get_format_instructions())
+
+agent = create_tool_calling_agent(
+    llm=llm,
+    prompt=prompt,
+    tools=[],    
+)
+
+agent_executor = AgentExecutor(agent=agent, tools=[], verbose=True)
+raw_response = agent_executor.invoke({"input": "What is the capital of France?"})
+print(raw_response)
 # completion = llm.chat.completions.create(
 #     model="deepseek/deepseek-chat",
 #     messages=[
